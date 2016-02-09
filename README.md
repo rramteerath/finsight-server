14937c356b516402826a0671b15b5c2d75e1522c
Raj Ramteerath <raj002@gmail.com>
2016-02-08 21:10:39 -0500

    updated readme and CORS

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 finsight api server using epilogie and sequelize
 
+App to learn React with more than just a trivial example.  
--- server.js
+++ server.js
@@ -25,10 +25,10 @@ router.use(function(req, res, next) {
 });
 
 app.all('*', function(req, res, next) {
-       res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-       res.header("Access-Control-Allow-Headers", "X-Requested-With");
-       res.header('Access-Control-Allow-Headers', 'Content-Type');
-       next();
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
 });
 
 // Define database
